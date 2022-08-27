817b0a437602f8553c90346b064910f552308723
Igor Magalhães <igortrapp30@gmail.com>
2022-08-27 08:51:01 -0400

    bump working

--- src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,17 +1,33 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../../../assets/CartIcon/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 import CartContext from "../../../store/CartContext";
 
 const HeaderCartButton = (props) => {
+  const [btnHighlighted, setBtnHighlighted] = useState(false);
   const cartContext = useContext(CartContext);
+  const { items } = cartContext;
 
-  const cartItemsQty = cartContext.items.reduce((counter, item) => {
+  const cartItemsQty = items.reduce((counter, item) => {
     return counter + item.qty;
   }, 0);
 
+  const btnStyles = `${styles.button} ${btnHighlighted ? styles.bump : ""}`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnHighlighted(true);
+    const btnTimer = setTimeout(() => {
+      setBtnHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(btnTimer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnStyles} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
